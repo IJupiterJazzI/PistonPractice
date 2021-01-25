39316ebc1d77b34626e3e0036482605358517631
Ava Byrd <avabyrd96@gmail.com>
2021-01-25 13:51:45 -0500

    Linear Actuator Started

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import frc.robot.subsystems.LaserSubsystem;
 import frc.robot.subsystems.PneumaticSubsystem;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -13,6 +13,12 @@ public class RobotMap {
     public static final int LEFT_INTAKE = 21;
     public static final int RIGHT_INTAKE = 22;
 
+    public static final int VERT_ACTUATOR = 13;
+    public static final int BOOM_ACTUATOR = 23;
+
     public static WPI_TalonSRX leftIntakeWheel = new WPI_TalonSRX(LEFT_INTAKE);
     public static WPI_TalonSRX rightIntakeWheel = new WPI_TalonSRX(RIGHT_INTAKE);
+
+    public static WPI_TalonSRX vertLinearActuatorMotor = new WPI_TalonSRX(VERT_ACTUATOR);
+    public static WPI_TalonSRX boomLinearActuatorMotor = new WPI_TalonSRX(BOOM_ACTUATOR);
 }
